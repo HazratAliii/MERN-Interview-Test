e7af8425e633a42d0902eba9dbc15e21a6d80d29
HazratAli <hazratali3711@gmail.com>
2024-09-15 17:03:56 +0600

    fixing deployment issue for frontend

--- whiteboard-fe/vite.config.js
+++ whiteboard-fe/vite.config.js
@@ -1,10 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/", // Root path for assets
   plugins: [react()],
-  base: "/",
   build: {
     outDir: "dist",
   },
